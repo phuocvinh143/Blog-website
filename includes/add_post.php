0116ed0f6a1823d5cf4f94e28718d6d184f0baf6
phuocvinh143 <maiphuocvinh143@gmail.com>
2021-05-17 23:53:18 +0700

    docker learning

--- includes/add_post.php
+++ includes/add_post.php
@@ -1,13 +1,15 @@
 <?php
 	require('db.inc.php');
 	require('function.php');
+	echo $_POST['add_post'];
 	if(isset($_POST['add_post'])) {
 		$ptitle = $conn->real_escape_string($_POST['post_title']);
 		$pcontent = $conn->real_escape_string($_POST['post_content']);
+		
 		$cid = $_POST['post_category'];
 		$admin = getAdminInfo($conn, $_SESSION['email']);
 		$aid = $admin['id'];
-
+		
 		$query = "INSERT INTO posts(title, content, category_id, admin_id) VALUES ('$ptitle', '$pcontent', '$cid', '$aid');";
 
 		$run = $conn->query($query);
@@ -19,7 +21,7 @@
 		$image_name = $_FILES['post_image']['name'];
 		$image_tmp = $_FILES['post_image']['tmp_name'];
 
-		// print_r($_FILES['post_image']);
+		print_r($_FILES['post_image']);
 		foreach ($_FILES["post_image"]["tmp_name"] as $key => $tmp_name){
 			move_uploaded_file($_FILES['post_image']['tmp_name'][$key], '../images/' . $_FILES['post_image']['name'][$key]);
 			$query = "INSERT INTO images(post_id, image) VALUES ('$post_id', '$image_name[$key]');";
@@ -28,4 +30,5 @@
 
 		header('location:../post.php?id='. $post_id);
 	}
+
 ?>
--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,13 +1,9 @@
 <?php
 	session_start();
-	$dbServername = "localhost";
-	$dbUsername = "root";
-	$dbPassword = "";
-	$dbName = "myblog";
+	$host='db';
+    $user='root';
+    $password='root';
+    $db='myblog';
 
-	$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-
-	if($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
+    $conn = new mysqli($host, $user, $password, $db);
 ?>
