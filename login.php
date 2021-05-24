b0c8a4f2fd558239da017694edb02f36fc7abe2d
Vinh Mai <maiphuocvinh143@gmail.com>
2021-05-24 22:31:07 +0700

    update css

--- login.php
+++ login.php
@@ -27,7 +27,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Blogbook - Sign In</title>
   <link rel="icon" href="images/icon.jpg" type="image/x-icon">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" />
+  <link href="styles/style.css" rel="stylesheet">
   <style>
     :root{
       --main-bg:white;
